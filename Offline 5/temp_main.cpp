fa380e4b841ea75f12d4370ddb3c4debe2813f72
Sadif-Ahmed <76277491+Sadif-Ahmed@users.noreply.github.com>
2022-01-13 13:46:33 +0600

    Heap and Heapsort Working with bugs :3

---
+++ Offline 5/temp_main.cpp
@@ -0,0 +1,21 @@
+/*
+#include<iostream>
+#include "heap.h"
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> temp;
+    int x;
+    for(int i=0;i<10;i++)
+    { cin>>x;
+      temp.push_back(x);
+    }
+    heapsort(temp);
+
+    for(int i=0;i<10;i++)
+    {
+        cout<<temp[i]<<" ";
+    }
+}
+*/
